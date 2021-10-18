5fe33cbda6fa6757da426dd08df76d42335bb638
zer0day <chumbaev.d@yandex.ru>
2021-10-18 15:31:46 +0300

    Add files via upload

---
+++ Update_uninstall.bat
@@ -0,0 +1,6 @@
+wusa /uninstall /kb:5005565 /queit
+wusa /uninstall /kb:5005564 /queit
+wusa /uninstall /kb:5005573 /queit
+wusa /uninstall /kb:5006670 /queit
+wusa /uninstall /kb:5006669 /queit
+pause
